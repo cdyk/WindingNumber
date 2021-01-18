a0fe4d6245fe96a30b61db1eb84534ac7b2322fb
Christopher Dyken <cdyk@users.noreply.github.com>
2021-01-18 12:34:28 +0100

    Move non-parallel for into header so compiler sees the code

--- ParallelFor.cpp
+++ ParallelFor.cpp
@@ -1,23 +1,2 @@
 #include "ParallelFor.h"
 
-void ParallelFor(size_t count, std::function<void(size_t)> func, size_t min_chunk)
-{
-  for (size_t i = 0; i < count; i++) {
-    func(i);
-  }
-}
-
-void ParallelFor(size_t count,
-                 std::function<void(size_t)> prep_func,
-                 std::function<void(size_t, size_t)> func,
-                 std::function<void(size_t)> accum_func, size_t min_chunk)
-{
-  size_t n = 32;
-  prep_func(n);
-  for (size_t i = 0; i < count; i++) {
-    func(i, i % n);
-  }
-  for (size_t i = 0; i < n; i++) {
-    accum_func(i);
-  }
-}
